7623a8e638377004bc00c0a97725536f26af8b9c
Adrian Moisey <adrian@changeover.za.net>
2016-09-26 21:09:35 +0200

    Naming things

--- github-deploy.py
+++ github-deploy.py
@@ -9,15 +9,15 @@ user = os.getenv('GITHUB_USER')
 repo = os.getenv('GITHUB_REPO')
 branch = os.getenv('GITHUB_BRANCH')
 
-environment = os.getenv('GITHUB_ENVIRONMENT')
-action = os.getenv('GITHUB_ACTION', 'create_deployment')
+environment = os.getenv('GITHUB_DEPLOYMENT_ENVIRONMENT')
+action = os.getenv('GITHUB_DEPLOYMENT_ACTION', 'create')
 deployment = os.getenv('GITHUB_DEPLOYMENT_ID', None)
 url = os.getenv('GITHUB_DEPLOYMENT_URL', None)
 
 gh = github3.login(token=token)
 repo = gh.repository(user, repo)
 
-if action == 'create_deployment':
+if action == 'create':
     deployment = repo.create_deployment(
         branch,
         auto_merge=True,
@@ -26,6 +26,6 @@ if action == 'create_deployment':
         deployment.create_status('pending', target_url=url)
     print(deployment.id)
 
-if action == 'successful_deployment' and deployment is not None:
+if action == 'successful' and deployment is not None:
     deployment = repo.deployment(deployment)
     deployment.create_status('success')
