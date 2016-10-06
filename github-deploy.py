c2f38a62b3715d2d9b52761f92471405ffd9a813
Adrian Moisey <adrian@changeover.za.net>
2016-10-06 09:05:16 +0200

    Refactor repo

--- github-deploy.py
+++ github-deploy.py
@@ -6,7 +6,7 @@ import github3
 # required
 token = os.getenv('GITHUB_TOKEN')
 user = os.getenv('GITHUB_USER')
-repo = os.getenv('GITHUB_REPO')
+repo_name = os.getenv('GITHUB_REPO')
 branch = os.getenv('GITHUB_BRANCH')
 
 environment = os.getenv('GITHUB_DEPLOYMENT_ENVIRONMENT')
@@ -14,11 +14,11 @@ action = os.getenv('GITHUB_DEPLOYMENT_ACTION', 'create')
 deployment_id = os.getenv('GITHUB_DEPLOYMENT_ID', None)
 url = os.getenv('GITHUB_DEPLOYMENT_URL', None)
 
-gh = github3.login(token=token)
-repo = gh.repository(user, repo)
+def repo(token, user, repo_name):
+    gh = github3.login(token=token)
+    return gh.repository(user, repo_name)
 
-
-def create_deployment(branch, environment, url=False, auto_merge=True):
+def create_deployment(repo, branch, environment, url=False, auto_merge=True):
     deployment = repo.create_deployment(
         branch,
         auto_merge=auto_merge,
@@ -28,13 +28,14 @@ def create_deployment(branch, environment, url=False, auto_merge=True):
     return(deployment.id)
 
 
-def update_deployment(action, deploymet_id):
+def update_deployment(repo, action, deploymet_id):
     deployment = repo.deployment(deployment_id)
     deployment.create_status(action)
 
 
 if __name__ == '__main__':
+    repo = repo(token, user, repo_name)
     if action == 'create':
-        print(create_deployment(branch, environment, url))
+        print(create_deployment(repo, branch, environment, url))
     if action == 'success' and deployment_id is not None:
-        update_deployment(action, deployment_id)
+        update_deployment(repo, action, deployment_id)
